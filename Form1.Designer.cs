ef447651bc92305c322373fb17d8797cc7d84557
Josh Hickman <josh.hickman@lethbridgecollege.ca>
2022-08-02 16:17:41 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SolverTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.solveButton = new System.Windows.Forms.Button();
+            this.unsolvedEquation = new System.Windows.Forms.Label();
+            this.solvedEquation = new System.Windows.Forms.Label();
+            this.postEquationLabel = new System.Windows.Forms.Label();
+            this.solvedIndecesLabel = new System.Windows.Forms.Label();
+            this.multiplicationLabel = new System.Windows.Forms.Label();
+            this.divisionLabel = new System.Windows.Forms.Label();
+            this.additionLabel = new System.Windows.Forms.Label();
+            this.subtractionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // solveButton
+            // 
+            this.solveButton.Location = new System.Drawing.Point(187, 190);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(75, 23);
+            this.solveButton.TabIndex = 0;
+            this.solveButton.Text = "Solve";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.solveButton_Click);
+            // 
+            // unsolvedEquation
+            // 
+            this.unsolvedEquation.AutoSize = true;
+            this.unsolvedEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unsolvedEquation.Location = new System.Drawing.Point(94, 97);
+            this.unsolvedEquation.Name = "unsolvedEquation";
+            this.unsolvedEquation.Size = new System.Drawing.Size(0, 25);
+            this.unsolvedEquation.TabIndex = 1;
+            // 
+            // solvedEquation
+            // 
+            this.solvedEquation.AutoSize = true;
+            this.solvedEquation.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solvedEquation.Location = new System.Drawing.Point(598, 97);
+            this.solvedEquation.Name = "solvedEquation";
+            this.solvedEquation.Size = new System.Drawing.Size(18, 25);
+            this.solvedEquation.TabIndex = 2;
+            this.solvedEquation.Text = " ";
+            // 
+            // postEquationLabel
+            // 
+            this.postEquationLabel.AutoSize = true;
+            this.postEquationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postEquationLabel.Location = new System.Drawing.Point(240, 140);
+            this.postEquationLabel.Name = "postEquationLabel";
+            this.postEquationLabel.Size = new System.Drawing.Size(0, 25);
+            this.postEquationLabel.TabIndex = 2;
+            // 
+            // solvedIndecesLabel
+            // 
+            this.solvedIndecesLabel.AutoSize = true;
+            this.solvedIndecesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solvedIndecesLabel.Location = new System.Drawing.Point(182, 239);
+            this.solvedIndecesLabel.Name = "solvedIndecesLabel";
+            this.solvedIndecesLabel.Size = new System.Drawing.Size(0, 25);
+            this.solvedIndecesLabel.TabIndex = 2;
+            // 
+            // multiplicationLabel
+            // 
+            this.multiplicationLabel.AutoSize = true;
+            this.multiplicationLabel.Location = new System.Drawing.Point(64, 275);
+            this.multiplicationLabel.Name = "multiplicationLabel";
+            this.multiplicationLabel.Size = new System.Drawing.Size(26, 13);
+            this.multiplicationLabel.TabIndex = 3;
+            this.multiplicationLabel.Text = "mult";
+            // 
+            // divisionLabel
+            // 
+            this.divisionLabel.AutoSize = true;
+            this.divisionLabel.Location = new System.Drawing.Point(64, 307);
+            this.divisionLabel.Name = "divisionLabel";
+            this.divisionLabel.Size = new System.Drawing.Size(28, 13);
+            this.divisionLabel.TabIndex = 3;
+            this.divisionLabel.Text = "divis";
+            // 
+            // additionLabel
+            // 
+            this.additionLabel.AutoSize = true;
+            this.additionLabel.Location = new System.Drawing.Point(64, 334);
+            this.additionLabel.Name = "additionLabel";
+            this.additionLabel.Size = new System.Drawing.Size(25, 13);
+            this.additionLabel.TabIndex = 3;
+            this.additionLabel.Text = "add";
+            // 
+            // subtractionLabel
+            // 
+            this.subtractionLabel.AutoSize = true;
+            this.subtractionLabel.Location = new System.Drawing.Point(64, 366);
+            this.subtractionLabel.Name = "subtractionLabel";
+            this.subtractionLabel.Size = new System.Drawing.Size(30, 13);
+            this.subtractionLabel.TabIndex = 3;
+            this.subtractionLabel.Text = "subtr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 491);
+            this.Controls.Add(this.subtractionLabel);
+            this.Controls.Add(this.additionLabel);
+            this.Controls.Add(this.divisionLabel);
+            this.Controls.Add(this.multiplicationLabel);
+            this.Controls.Add(this.solvedIndecesLabel);
+            this.Controls.Add(this.postEquationLabel);
+            this.Controls.Add(this.solvedEquation);
+            this.Controls.Add(this.unsolvedEquation);
+            this.Controls.Add(this.solveButton);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.Label unsolvedEquation;
+        private System.Windows.Forms.Label solvedEquation;
+        private System.Windows.Forms.Label postEquationLabel;
+        private System.Windows.Forms.Label solvedIndecesLabel;
+        private System.Windows.Forms.Label multiplicationLabel;
+        private System.Windows.Forms.Label divisionLabel;
+        private System.Windows.Forms.Label additionLabel;
+        private System.Windows.Forms.Label subtractionLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolverTest {    //**NB(pretty outdated)** I think the error at the moment is that its not passing its value forward if an operator with higher precedence that has already been solved and should be skipped is in the way. Index should keep incrementing until it finds an element that contains the operator
+    public partial class Form1 : Form {     //I think I can solve my problems by keeping a list of solved equations indeces and deleting in a single for loop at function end to avoid indexing issues mid operation (problem below).
+        public Form1() {
+            InitializeComponent();
+        }
+                                    //, "2+2", "2/2", "2/4", "4-2", "2+3", "3+4", "4*4", "4+5", "5*6" 
+        List<string> equation = new List<string> { "2/3", "3+3", "3-3", "3*3", "3/3" };//This still breaks when duplicate operations are adjacent to eachother, the value is not passed correctly because the previous solved values are not deleted until the end
+        List<int> solvedIndeces = new List<int>();
+        string result = "";                                                     //I think I can solve the above problem by creating a do while loop that checks if the previous element contains the current operator (we will only ever need to look backwards because equations are solved left to right) and replacing the first one not containing it/terminating the loop. 
+    
+        private void solveButton_Click(object sender, EventArgs e) {    //***NB*****  I THINK THE BEST PLAN IS TO TEST AND FIX USING PRINT OUTS OF BROOKEN EQUATIONS
+            Solver(equation, '*', '/');                                      //**NB***** MD and AS must be done simultaneously left to right 2 When I do the math by hand it is correct but calculators disagree and adding a minus to the end breaks it
+            for (int i = 0; i < equation.Count; i++) { multiplicationLabel.Text += " " + equation[i];}
+            for (int i = solvedIndeces.Count - 1; i > -1; i--) {
+                equation.RemoveAt(solvedIndeces[i]);    //TODO turn this into a cleanup function
+            }
+            solvedIndeces.Clear();
+
+            Solver(equation, '+', '-');
+            for (int i = 0; i < equation.Count; i++) { additionLabel.Text += " " + equation[i]; }
+            for (int i = solvedIndeces.Count - 1; i > -1; i--) {
+                equation.RemoveAt(solvedIndeces[i]);
+            }
+            solvedIndeces.Clear();
+
+            for (int i = 0; i < equation.Count; i++) {
+                postEquationLabel.Text += equation[i] + " ";
+            }
+            for (int i = 0; i < solvedIndeces.Count; i++) {
+                solvedIndecesLabel.Text += solvedIndeces[i].ToString();
+            }
+            solvedEquation.Text += result + " ";
+        }
+
+        private void Solver(List<string> equation, char operator1, char operator2) {
+
+            for (int i = 0; i < equation.Count; i++) {
+
+                if (equation[i].Contains(operator1)) {
+
+                    solvedIndeces.Add(i);
+                    string[] operands = equation[i].Split(operator1);
+
+                    switch (operator1) {
+                        case '*':
+                            result = (double.Parse(operands[0]) * double.Parse(operands[1])).ToString();
+                            break;
+                        case '/':
+                            result = (double.Parse(operands[0]) / double.Parse(operands[1])).ToString();
+                            break;
+                        case '+':
+                            result = (double.Parse(operands[0]) + double.Parse(operands[1])).ToString();
+                            break;
+                        case '-':
+                            result = (double.Parse(operands[0]) - double.Parse(operands[1])).ToString();
+                            break;
+                    }
+
+                    if (i != equation.Count - 1) {
+                        equation[i + 1] = replaceFirstOccurance(equation[i + 1], operands[1], result);//equation[i + 1].Replace(operands[1], result); //These are replacing ALL instances of the old value found in the string, breaks on duplicate operands
+                    }
+
+                    if (i != 0) {
+                        for (int passerIndex = i -1; passerIndex >= 0; passerIndex--) { //**NB** This seems to be causing trouble 
+                            if (!((equation[passerIndex].Contains(operator1)) || (equation[passerIndex].Contains(operator2)))) {
+                                equation[passerIndex] = replaceLastOccurance(equation[passerIndex], operands[0], result);// equation[i - 1].Replace(operands[0], result);
+                                break;
+                            }
+                        }
+                    }
+
+                } else if (equation[i].Contains(operator2)) {
+
+                    solvedIndeces.Add(i);
+                    string[] operands = equation[i].Split(operator2);
+
+                    switch (operator2) {
+                        case '*':
+                            result = (double.Parse(operands[0]) * double.Parse(operands[1])).ToString();
+                            break;
+                        case '/':
+                            result = (double.Parse(operands[0]) / double.Parse(operands[1])).ToString();
+                            break;
+                        case '+':
+                            result = (double.Parse(operands[0]) + double.Parse(operands[1])).ToString();
+                            break;
+                        case '-':
+                            result = (double.Parse(operands[0]) - double.Parse(operands[1])).ToString();
+                            break;
+                    }
+
+                    if (i != equation.Count - 1) {
+                        equation[i + 1] = replaceFirstOccurance(equation[i + 1], operands[1], result);//equation[i + 1].Replace(operands[1], result); //These are replacing ALL instances of the old value found in the string, breaks on duplicate operands
+                    }
+
+                    if (i != 0) {
+                        for (int passerIndex = i - 1; passerIndex >= 0; passerIndex--) {
+                            if (!((equation[passerIndex].Contains(operator2)) || (equation[passerIndex].Contains(operator1)))) {
+                                equation[passerIndex] = replaceLastOccurance(equation[passerIndex], operands[0], result);// equation[i - 1].Replace(operands[0], result);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private string replaceFirstOccurance(string original, string operand, string result) {
+            int loc = original.IndexOf(operand);
+            if (loc != -1) {        //checks if it is there first
+                Console.WriteLine("firstWorks");
+                return original.Remove(loc, operand.Length).Insert(loc, result); 
+            } else {Console.WriteLine("first");return original; }
+        }
+
+        private string replaceLastOccurance(string original, string operand, string result) {
+            int loc = original.LastIndexOf(operand);
+            if (loc != -1) {        //checks if it is there first
+                Console.WriteLine("lastWorks");
+                return original.Remove(loc, operand.Length).Insert(loc, result); 
+            } else { Console.WriteLine("last"); return original; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            for (int i = 0; i < equation.Count; i++) {
+                unsolvedEquation.Text += equation[i] + " ";
+            }
+        }
+    }
+}
